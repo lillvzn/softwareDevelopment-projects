3072796c13b31c802e10217c2336b80bf5d41db1
Punith <punith.p@icloud.com>
2021-10-14 19:08:29 +0530

    JS challenges

--- codewars/sum-of-digits.js
+++ codewars/sum-of-digits.js
@@ -6,4 +6,4 @@ function digital_root(n) {
   const remainDigits = Math.floor(n / 10);
   return digital_root(lastDigit + digital_root(remainDigits));
 }
-//https://www.codewars.com/kata/541c8630095125aba6000c00
+// https://www.codewars.com/kata/541c8630095125aba6000c00
