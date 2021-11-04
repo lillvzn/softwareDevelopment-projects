c67758fc9cae4eae3cec958dd38ed9def23bdbea
Punith <punith.p@icloud.com>
2021-11-04 16:20:53 +0530

    random/self clarification

---
+++ challenges/random/arraySort.js
@@ -0,0 +1,15 @@
+"use strict";
+
+const sortThisArray = function (arr, order = "asc") {
+  const arrLen = arr.length;
+  const res = [];
+  for (let i = 0; i < arrLen; i++) {
+    let minVal = Math.min(...arr);
+    res.push(minVal);
+    arr.splice(arr.indexOf(minVal), 1);
+  }
+  if (order === "dsc") return res.reverse();
+  return res;
+};
+
+sortThisArray([5, 3, 17, 24, 1, 7, 14, -2, 0], "dsc");
